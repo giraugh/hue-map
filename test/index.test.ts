cafe123380070d851810f7da2014f7672ab3417c
Ewan Breakey <ebreakey@gmail.com>
2022-08-20 16:41:56 +1000

    Refactor tests to use describe/it semantics

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,25 +1,49 @@
-import { expect, test } from 'vitest'
+import { expect, it, describe } from 'vitest'
 import defaultExport, { createPalette, convertRGBA, maps } from '../lib'
 import mapsFromFile from '../lib/maps'
 
-test('exports', () => {
-  expect(mapsFromFile).toBe(maps)
-  expect(defaultExport).toBe(createPalette)
+describe('exports', () => {
+  it('has correct maps export', () => expect(mapsFromFile).toBe(maps))
+  it('has correct default export', () => expect(defaultExport).toBe(createPalette))
 })
 
-test('convertRGBA', () => {
-  expect(convertRGBA([247,158,0,255], 'cssHex')).toStrictEqual('#F79E00FF')
-  expect(convertRGBA([247,158,0,255], 'cssRGBA')).toStrictEqual('rgba(247, 158, 0, 255)')
-  expect(convertRGBA([247,158,0,255], 'float')).toStrictEqual([0.9686274509803922,0.6196078431372549,0,1])
-  expect(convertRGBA([247,158,0,255], 'rgba')).toStrictEqual([247,158,0,255])
-  expect(convertRGBA([247,158,0,255], 'number')).toStrictEqual(0xF79E00FF)
+describe('convertRGBA', () => {
+  it('converts to cssHex correctly', () => expect(convertRGBA([247,158,0,255], 'cssHex')).toStrictEqual('#F79E00FF'))
+  it('converts to cssRGBA correctyl', () => expect(convertRGBA([247,158,0,255], 'cssRGBA')).toStrictEqual('rgba(247, 158, 0, 255)'))
+  it('converts to float correctly', () => expect(convertRGBA([247,158,0,255], 'float')).toStrictEqual([0.9686274509803922,0.6196078431372549,0,1]))
+  it('converts to rgba correctly', () => expect(convertRGBA([247,158,0,255], 'rgba')).toStrictEqual([247,158,0,255]))
+  it('converts to number correctly', () => expect(convertRGBA([247,158,0,255], 'number')).toStrictEqual(0xF79E00FF))
+
+  it('throws an exception for unknown formats', () => {
+    // @ts-ignore
+    expect(() => convertRGBA([0, 0, 0, 0], '')).toThrow()
+  })
 })
 
-test('createPalette', () => {
-  expect(createPalette()).toStrictEqual(['#440154FF','#472C7AFF','#413F83FF','#3B518BFF','#2C718EFF','#21908DFF','#27AD81FF','#42BB72FF','#5CC863FF','#AADC32FF'])
-  expect(createPalette({ steps: 2 })).toStrictEqual(['#472C7AFF','#27AD81FF'])
-  expect(createPalette({ map: 'jet', steps: 5 })).toStrictEqual(['#000083FF','#003CAAFF','#05FFFFFF','#FFFF00FF','#FA0000FF'])
-  expect(createPalette({ map: 'alpha', steps: 5 })).toStrictEqual(['#FFFFFF00','#FFFFFF33','#FFFFFF66','#FFFFFF99','#FFFFFFCC'])
-  expect(createPalette({ map: 'plasma', steps: 5, format: 'number' })).toStrictEqual([0x0D0887FF,0x7D03A8FF,0xA82296FF,0xE56B5DFF,0xFDC328FF])
-  expect(createPalette({ steps: 100 }).length).toBe(100)
+describe('createPalette', () => {
+  it('creates the correct palette using default arguments', () => {
+    expect(createPalette())
+      .toStrictEqual(['#440154FF','#472C7AFF','#413F83FF','#3B518BFF','#2C718EFF','#21908DFF','#27AD81FF','#42BB72FF','#5CC863FF','#AADC32FF'])
+  })
+
+  it('creates the correct number of color steps', () => {
+    expect(createPalette({ steps: 1 })).toHaveLength(1)
+    expect(createPalette({ steps: 50 })).toHaveLength(50)
+    expect(createPalette({ steps: 100 })).toHaveLength(100)
+  })
+
+  it('creates the correct palette', () => {
+    expect(createPalette({ map: 'jet', steps: 5 }))
+      .toStrictEqual(['#000083FF','#003CAAFF','#05FFFFFF','#FFFF00FF','#FA0000FF'])
+  })
+
+  it('correctly interpolates alpha', () => {
+    expect(createPalette({ map: 'alpha', steps: 5 }))
+      .toStrictEqual(['#FFFFFF00','#FFFFFF33','#FFFFFF66','#FFFFFF99','#FFFFFFCC'])
+  })
+
+  it('returns the correct color format', () => {
+    expect(createPalette({ map: 'plasma', steps: 5, format: 'number' }))
+      .toStrictEqual([0x0D0887FF,0x7D03A8FF,0xA82296FF,0xE56B5DFF,0xFDC328FF])
+  })
 })
--- test/maps.test.ts
+++ test/maps.test.ts
@@ -1,10 +1,12 @@
-import { expect, test } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import maps from '../lib/maps'
 
-test('valid', () => {
-  // Index is between 0 and 1
-  expect(Object.values(maps).every(map => map.every(([index]) => index >= 0 && index <= 1))).toBeTruthy()
+describe('maps', () => {
+  it('has indices between 0 and 1', () => {
+    expect(Object.values(maps).flat().every(([index]) => index >= 0 && index <= 1)).toBeTruthy()
+  })
 
-  // Colour is between 0 and 0xFFFFFFFF
-  expect(Object.values(maps).every(map => map.every(([, color]) => color >= 0 && color <= 0xFFFFFFFF))).toBeTruthy()
+  it('has colour values between 0 and 0xFFFFFFFF', () => {
+    expect(Object.values(maps).flat().every(([, color]) => color >= 0 && color <= 0xFFFFFFFF)).toBeTruthy()
+  })
 })
