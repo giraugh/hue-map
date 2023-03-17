a69f51fc52c72fad0e5c2c38b445866c7ba9c26d
giraugh <giraugh@users.noreply.github.com>
2023-03-17 00:18:57 +0000

    deploy: 054a91dff37dbe4090b767768dc5c9c90ff1d98d

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,23 @@
 import maps, { ColorMap, MapKey } from './maps';
-import { RGBA } from './util';
-export type Palette = string[] | RGBA[] | number[];
+import { RGBA, RGBAFloat } from './util';
 export type ColorMapInput = MapKey | ColorMap;
 export type PaletteFormat = 'float' | 'rgba' | 'cssHex' | 'cssRGBA' | 'number';
 type CreatePaletteOptions = {
     map?: ColorMapInput;
     steps?: number;
-    format?: PaletteFormat;
 };
+declare class Palette {
+    private readonly colors;
+    constructor(colors: RGBA[]);
+    /** Convert this palette into a specified format */
+    format(_format: 'cssRGBA'): string[];
+    format(_format: 'number'): number[];
+    format(_format: 'float'): RGBAFloat[];
+    format(_format: 'rgba'): RGBA[];
+    format(_format?: 'cssHex'): string[];
+    /** Get the number of colors in this palette */
+    get length(): number;
+}
 /**
  * Generate a palette from a given color map.
  * @param {ColorMapInput} options.map The name of the colormap to use, or a custom map.
@@ -15,7 +25,7 @@ type CreatePaletteOptions = {
  * @param {PaletteFormat} options.format The format of the palette colors.
  * @returns {Palette} The generated palette.
  */
-export declare const createPalette: ({ map, steps, format }?: CreatePaletteOptions) => Palette;
+export declare const createPalette: ({ map, steps }?: CreatePaletteOptions) => Palette;
 /**
  * Convert RGBA tuple to specified color format
  * @param {RGBA} rgba rgba tuple to convert
